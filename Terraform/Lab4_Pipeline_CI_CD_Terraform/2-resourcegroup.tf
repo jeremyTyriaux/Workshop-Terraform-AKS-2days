9a46097abaeefa245e6d965e37355c5d35fc67fe
jeremy tyriaux <jeremy.tyriaux@gmail.com>
2022-06-15 07:52:31 +0200

    Update 2-resourcegroup.tf

--- Terraform/Lab4_Pipeline_CI_CD_Terraform/2-resourcegroup.tf
+++ Terraform/Lab4_Pipeline_CI_CD_Terraform/2-resourcegroup.tf
@@ -12,3 +12,17 @@ resource "azurerm_resource_group" "Terra_tfbackend_rg" {
   name     = var.resourceGroupName
   location = var.azureRegion
 }
+
+
+
+resource "azurerm_storage_account" "Terra-Storage-Backend" {
+  name                      = "blabla"
+  resource_group_name       = azurerm_resource_group.Terra_tfbackend_rg.name
+  location                  = azurerm_resource_group.Terra_tfbackend_rg.location
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+  account_kind              = "StorageV2"
+  enable_https_traffic_only = true
+}
+
+
